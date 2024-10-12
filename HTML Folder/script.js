b40ca456574d12ebd0a04dd293d61948f271f428
rishitmakadia <rishitmakadia@gmail.com>
2024-10-12 15:32:28 +0530

    separated JS file

---
+++ HTML Folder/script.js
@@ -0,0 +1,16 @@
+// Login form JS
+function showLoginForm() {
+    document.getElementById("nithesh").style.display = "block";
+    document.getElementById("LoginForm").style.display = "block";
+    document.getElementById("SignupForm").style.display = "none";
+    document.getElementById("navigation").style.display = "none";
+    document.getElementById("navigation2").style.display="none";
+}
+// SignUp form JS
+function showSignUpForm(){
+    document.getElementById("nithesh").style.display = "block";
+    document.getElementById("LoginForm").style.display = "none";
+    document.getElementById("SignupForm").style.display = "block";
+    document.getElementById("navigation").style.display = "none";
+    document.getElementById("navigation2").style.display="none";
+}
