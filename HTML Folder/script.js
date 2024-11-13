3b0581c53ba178ca246ca55ef55f0e07eb222c17
Nithesh <nitheshu944@gmail.com>
2024-11-13 23:31:41 +0530

    Correction to make wrapper appear only during login and signup form

--- HTML Folder/script.js
+++ HTML Folder/script.js
@@ -2,9 +2,11 @@
 function showLoginForm() {
     document.getElementById("LoginForm").style.display = "block";
     document.getElementById("SignupForm").style.display = "none";
+    document.getElementById("wrapped").style.display ="block";
 }
 
 function showSignUpForm() {
     document.getElementById("LoginForm").style.display = "none";
     document.getElementById("SignupForm").style.display = "block";
+    document.getElementById("wrapped").style.display ="block";
 }
